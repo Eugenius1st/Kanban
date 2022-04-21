ade3cc5cc9f337660ff7b049cb4760b2ea1a4a86
안유진 <Eugenius1st@gmail.com>
2022-04-22 01:56:06 +0900

    [#4]: Drags and Drop part One
    - Drag and Drop을 활용하기 위한 초기 설정

--- kanban/src/App.tsx
+++ kanban/src/App.tsx
@@ -1,27 +1,26 @@
-import React from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { hourSelector, minuteState } from "./atoms";
-//학습목표: set함수를 이용하여 hours도 바꿔보자 !
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+//학습목표: 드래그 앤 드롭 리스트를 만들자! npm i react-beautiful-dnd
+//Draggable(드래그 할 수 있는 영역) 과 Drappable(드롭할 수 있는 영역)이 있다. DragDropContext을 import 해주자
 
 function App() {
-    const [minutes, setMinutes] = useRecoilState(minuteState);
-    const [hours, setHours] = useRecoilState(hourSelector);
-    // recoilState는 결과로 array를 주는데 array의 첫번째 item은 atom 값이고
-    // 두 번재 argument는 atom을 수정하는 함수다.
-    // 첫 번째 요소는 get property로부터 return한 값
-    // 두 번째 요소는 setProperty를 부르는 함수가 될 것이다.
-    const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
-        setMinutes(+event.currentTarget.value);
-    };
-
-    const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
-        setHours(+event.currentTarget.value);
-    };
-
+    const onDragEnd = () => {};
     return (
-        <div>
-            <input onChange={onHoursChange} value={minutes} type="number" placeholder="Minutes" />
-        </div>
+        <DragDropContext onDragEnd={onDragEnd}>
+            <div>
+                <Droppable droppableId="one">
+                    {() => (
+                        <ul>
+                            <Draggable draggableId="first" index={0}>
+                                {() => <li>One</li>}
+                            </Draggable>
+                            <Draggable draggableId="second" index={1}>
+                                {() => <li>Two</li>}
+                            </Draggable>
+                        </ul>
+                    )}
+                </Droppable>
+            </div>
+        </DragDropContext>
     );
 }
 
--- kanban/src/atoms.tsx
+++ kanban/src/atoms.tsx
@@ -14,10 +14,5 @@ export const hourSelector = selector<number>({
     set: ({ set }, newValue) => {
         const minutes = Number(newValue) * 60;
         set(minuteState, minutes);
-        //set함수는 2개의 인자를 받는다.
-        //1. 수정하고 싶은 recoil atom을 가져오고
-        //2. 새로운 값인 minutes를 가져온다
     },
-    //set은 state를 set하는 것을 도와주는 속성이다. atom을 수정하는 것을 돕는다.
-    //atom은 input으로 부터 온다.
 });
