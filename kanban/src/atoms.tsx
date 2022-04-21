16fb117271ed1049bb4f61be57fc3000014a208d
안유진 <Eugenius1st@gmail.com>
2022-04-21 14:04:16 +0900

    [#2]: Get Selectors - input 과 selector 연결하기 - input 을 state로 가져와서 state를 수정하고 - input을 수정하여 output을 보낸다 -> 이것이 selector 이다.

--- kanban/src/atoms.tsx
+++ kanban/src/atoms.tsx
@@ -1,1 +1,16 @@
 import { atom, selector } from "recoil";
+
+export const minuteState = atom({
+    key: "minutes",
+    default: 0,
+});
+
+export const hourSelector = selector({
+    key: "hours",
+    get: ({ get }) => {
+        const minutes = get(minuteState);
+        //함수를 가져올 수도 있다.
+        return minutes / 60;
+    },
+    //여기서 어떤 값을 return하던지간에 그 값은 hourSelector의 값이 될 것이다.
+});
