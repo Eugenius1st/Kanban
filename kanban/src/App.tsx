defa8e2cde80d160c5167ab5ff3d30308278b651
안유진 <Eugenius1st@gmail.com>
2022-04-22 03:54:38 +0900

    [#21]: Performance

--- kanban/src/App.tsx
+++ kanban/src/App.tsx
@@ -3,7 +3,7 @@ import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { toDoState } from "./atoms";
 
-//todo State를 위한 atom 만들기
+//array를 재 정렬하는 splice
 
 const Wrapper = styled.div`
     display: flex;
@@ -40,9 +40,28 @@ const toDos = ["a", "b", "c", "d", "e", "f"];
 
 function App() {
     const [toDos, setToDos] = useRecoilState(toDoState);
-    const onDragEnd = ({ destination, source }: DropResult) => {
+    const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
+        if (!destination) return;
         //드래그가 끝났을 때 실행되는 함수이다. argument는 많은 정보를 준다.
         //index, destination 등등.. 이는 어디로 가는지 알 수 있다.
+        setToDos((toDosCopy) => {
+            //복사해서 기다린다.
+            const copyToDos = [...toDosCopy];
+            console.log("Delete item on", source.index);
+            console.log(toDosCopy);
+            toDosCopy.splice(source.index, 1);
+            console.log("Deleted item");
+            console.log(toDosCopy);
+            // 1. delete item on source.index
+            copyToDos.splice(source.index, 1);
+            // 2.put back the item on the destination.index 도착한 지점에 넣어준다
+            console.log("Put back", draggableId, "on ", destination.index);
+            toDosCopy.splice(destination?.index, 0, draggableId);
+            console.log(toDosCopy);
+            return copyToDos;
+
+            // 3.
+        });
     };
     //splice는 인덱스를 지우고 넣고싶은 item을 넣을 수 있다.(위치 지정 가능, item넣기 선택 가능)
     return (
@@ -53,7 +72,8 @@ function App() {
                         {(magic) => (
                             <Board ref={magic.innerRef} {...magic.droppableProps}>
                                 {toDos.map((toDo, index) => (
-                                    <Draggable draggableId={toDo} index={index}>
+                                    <Draggable key={toDo} draggableId={toDo} index={index}>
+                                        {/* key와 draggable은 같아야 한다 */}
                                         {(magic) => (
                                             <Card ref={magic.innerRef} {...magic.draggableProps} {...magic.dragHandleProps}>
                                                 {toDo}
