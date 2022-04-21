702569757312d307cf72ef575e10f2f6fe322ca8
안유진 <Eugenius1st@gmail.com>
2022-04-22 01:20:00 +0900

    [#3]: set Selector

--- kanban/src/App.tsx
+++ kanban/src/App.tsx
@@ -1,28 +1,26 @@
 import React from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { hourSelector, minuteState } from "./atoms";
-//학습목표: input과 selector 연결하기
+//학습목표: set함수를 이용하여 hours도 바꿔보자 !
 
 function App() {
     const [minutes, setMinutes] = useRecoilState(minuteState);
-    //useRecoilState는 atom값에 더해서 atom을 수정할 함수까지 준다.
-    const hours = useRecoilValue(hourSelector);
-    //useRecoilValue는 atom 값을 가져온다.
+    const [hours, setHours] = useRecoilState(hourSelector);
+    // recoilState는 결과로 array를 주는데 array의 첫번째 item은 atom 값이고
+    // 두 번재 argument는 atom을 수정하는 함수다.
+    // 첫 번째 요소는 get property로부터 return한 값
+    // 두 번째 요소는 setProperty를 부르는 함수가 될 것이다.
     const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
         setMinutes(+event.currentTarget.value);
-        //+를 붙여서 정수형으로 만든다.
+    };
+
+    const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+        setHours(+event.currentTarget.value);
     };
 
     return (
         <div>
-            <input value={minutes} onChange={onMinutesChange} type="number" placeholder="Minutes" />
-
-            <input value={hours} type="number" placeholder="Hours" />
-            {/* 기본적인 단위를 바꿔보는 작업을 해보자 
-            minutes* 값에 따라 Hours 값을 즉시 바꾸도록 하고싶다.
-            기억해봐라 state를 가져와서 그 state를 수정하고 output을 내보낼 때 어떤 것을 사용했나!*/}
-            {/* input 을 state로 가져오고 일련의 방법으로 input을 수정했고  수정한 값을  output으로 수정했다,
-            그것이 selector 이다. */}
+            <input onChange={onHoursChange} value={minutes} type="number" placeholder="Minutes" />
         </div>
     );
 }
--- kanban/src/atoms.tsx
+++ kanban/src/atoms.tsx
@@ -1,16 +1,23 @@
 import { atom, selector } from "recoil";
-
+//학습목표 : set 배우기
 export const minuteState = atom({
     key: "minutes",
     default: 0,
 });
 
-export const hourSelector = selector({
+export const hourSelector = selector<number>({
     key: "hours",
     get: ({ get }) => {
         const minutes = get(minuteState);
-        //함수를 가져올 수도 있다.
         return minutes / 60;
     },
-    //여기서 어떤 값을 return하던지간에 그 값은 hourSelector의 값이 될 것이다.
+    set: ({ set }, newValue) => {
+        const minutes = Number(newValue) * 60;
+        set(minuteState, minutes);
+        //set함수는 2개의 인자를 받는다.
+        //1. 수정하고 싶은 recoil atom을 가져오고
+        //2. 새로운 값인 minutes를 가져온다
+    },
+    //set은 state를 set하는 것을 도와주는 속성이다. atom을 수정하는 것을 돕는다.
+    //atom은 input으로 부터 온다.
 });
