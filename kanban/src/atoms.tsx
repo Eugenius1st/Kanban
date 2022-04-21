b2435876e27adf9c9786aaf5b6d28c952b472c35
안유진 <Eugenius1st@gmail.com>
2022-04-22 02:20:39 +0900

    [#5]: Drag and Drop part Two - 드래그 앤 드롭 기능 시연

--- kanban/src/atoms.tsx
+++ kanban/src/atoms.tsx
@@ -1,18 +1,1 @@
 import { atom, selector } from "recoil";
-//학습목표 : set 배우기
-export const minuteState = atom({
-    key: "minutes",
-    default: 0,
-});
-
-export const hourSelector = selector<number>({
-    key: "hours",
-    get: ({ get }) => {
-        const minutes = get(minuteState);
-        return minutes / 60;
-    },
-    set: ({ set }, newValue) => {
-        const minutes = Number(newValue) * 60;
-        set(minuteState, minutes);
-    },
-});
