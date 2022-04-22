d786f0e3f78ac38204b9f8551bdca5e9bf618817
안유진 <Eugenius1st@gmail.com>
2022-04-22 13:43:05 +0900

    [#26]: Droppable Snapshot

--- kanban/src/Components/Board.tsx
+++ kanban/src/Components/Board.tsx
@@ -9,6 +9,8 @@ const Wrapper = styled.div`
     background-color: ${(props) => props.theme.boardColor};
     border-radius: 5px;
     min-height: 300px;
+    display: flex;
+    flex-direction: column;
 `;
 
 const Title = styled.h2`
@@ -18,9 +20,21 @@ const Title = styled.h2`
     font-size: 18px;
 `;
 
+const Area = styled.div<IAreaProps>`
+    background-color: ${(props) => (props.isDraggingOver ? "pink" : props.isDraggingFromThis ? "red" : "skyblue")};
+    //div를 떠날때 색깓들을 바꿔준다.
+    flex-grow: 1;
+    transition: background-color 0.3s ease-in-out;
+`;
+
 interface IBoardProps {
     toDos: string[];
     boardId: string;
+}
+
+interface IAreaProps {
+    isDraggingFromThis: boolean;
+    isDraggingOver: boolean;
 }
 
 export default function Board({ toDos, boardId }: IBoardProps) {
@@ -28,13 +42,15 @@ export default function Board({ toDos, boardId }: IBoardProps) {
         <Wrapper>
             <Title>{boardId}</Title>
             <Droppable droppableId={boardId}>
-                {(magic) => (
-                    <div ref={magic.innerRef} {...magic.droppableProps}>
+                {(magic, snapshot) => (
+                    //snapshot을 오른쪽 마우스로 클릭 후 type defination을 통해 봐보자
+                    //drag의 from부터 to를 알 수 있다.
+                    <Area isDraggingOver={snapshot.isDraggingOver} isDraggingFromThis={Boolean(snapshot.draggingFromThisWith)} ref={magic.innerRef} {...magic.droppableProps}>
                         {toDos.map((toDo, index) => (
                             <DragabbleCard key={toDo} index={index} toDo={toDo} />
                         ))}
                         {magic.placeholder}
-                    </div>
+                    </Area>
                 )}
             </Droppable>
         </Wrapper>
